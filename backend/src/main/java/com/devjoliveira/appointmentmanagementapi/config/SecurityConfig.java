ee3f21955aca00f258137cc51a2327ae35e78f77
devjoliveira <macpro@MacBook-Pro-de-MacPro.local>
2026-03-09 23:22:12 -0300

    Create login and page appointments

--- backend/src/main/java/com/devjoliveira/appointmentmanagementapi/config/SecurityConfig.java
+++ backend/src/main/java/com/devjoliveira/appointmentmanagementapi/config/SecurityConfig.java
@@ -82,7 +82,12 @@ public class SecurityConfig {
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
     configuration.applyPermitDefaultValues();
+
+    configuration.setAllowedOrigins(List.of("http://localhost:4200"));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
+    configuration.setAllowedHeaders(List.of("*"));
+    configuration.setAllowCredentials(true);
+
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", configuration);
     return source;
