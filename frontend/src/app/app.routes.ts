0cb3a4a181f2ce7ece1510bfa89f3ae062a64d64
devjoliveira <macpro@MacBook-Pro-de-MacPro.local>
2026-03-09 22:42:08 -0300

    Create app routes ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login.component';
+import { AppointmentListComponent } from './features/appointments/pages/appointment-list/appointment-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'appointments', component: AppointmentListComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
