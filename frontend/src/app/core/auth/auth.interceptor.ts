e192e7af55ab1140ab752dafe0e7abc835b11149
devjoliveira <macpro@MacBook-Pro-de-MacPro.local>
2026-03-10 23:11:35 -0300

    Refact interceptor for expired token redirect to login

--- frontend/src/app/core/auth/auth.interceptor.ts
+++ frontend/src/app/core/auth/auth.interceptor.ts
@@ -1,11 +1,16 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
+import { catchError } from 'rxjs';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
 
   const authService = inject(AuthService);
   const token = authService.getToken();
+
+  const router = inject(Router);
+  let authReq = req;
 
   if (token) {
     req = req.clone({
@@ -15,5 +20,16 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
     });
   }
 
-  return next(req);
+  return next(authReq).pipe(
+    catchError((error) => {
+      if (error.status === 401 || error.status === 403) {
+        localStorage.removeItem('token');
+        router.navigate(['/login']);
+      }
+      throw error;
+    })
+  );
+
 }
+
+
