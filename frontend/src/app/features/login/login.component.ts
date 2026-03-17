b63359ffd860fa2a355cd7c9e61b601e185abc87
devjoliveira <macpro@MacBook-Pro-de-MacPro.local>
2026-03-17 20:11:14 -0300

    Fix: login with message error

--- frontend/src/app/features/login/login.component.ts
+++ frontend/src/app/features/login/login.component.ts
@@ -15,22 +15,56 @@ export class LoginComponent {
   email = "";
   password = "";
 
+  successMessage = '';
+  errorMessage = '';
+  successToast: any;
+  errorToast: any;
+
   constructor(
     private authService: AuthService,
     private router: Router
   ) { }
 
   login() {
+
     this.authService.login({
+
       email: this.email,
       password: this.password
-    }).subscribe(response => {
 
-      this.authService.saveToken(response.accessToken);
-
-      this.router.navigate(['/dashboard']);
-
+    }).subscribe({
+      next: (response) => {
+        this.showSuccess("Login efetuado com sucesso");
+        this.authService.saveToken(response.accessToken);
+        this.router.navigate(['/dashboard']);
+      },
+      error: (err) => {
+        this.showError('Erro ao efetuar o login. Tente novamente.');
+      }
     });
+  }
+
+  ngAfterViewInit() {
+    const successEl = document.getElementById('successToast');
+    const errorEl = document.getElementById('errorToast');
+
+    this.successToast = new (window as any).bootstrap.Toast(successEl, {
+      delay: 6000
+    });
+
+    this.errorToast = new (window as any).bootstrap.Toast(errorEl, {
+      delay: 4000
+    });
+  }
+
+  showSuccess(message: string) {
+    this.successMessage = message;
+    this.successToast.show();
+  }
+
+  showError(message: string) {
+    this.errorMessage = message;
+    this.errorToast.show();
   }
 
 }
